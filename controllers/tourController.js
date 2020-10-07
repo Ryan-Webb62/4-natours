0fd609ea48939a661e9e5f7128428393580d095a
Ryan Webb <65420906+Ryan-Webb62@users.noreply.github.com>
2020-10-07 10:32:10 -0400

    Refactor code for MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,4 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const Tour = require('../models/tourModels');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -14,53 +10,32 @@ exports.checkBody = (req, res, next) => {
   next();
 };
 
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid tour ID',
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.reqTime,
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours: tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+  // const tour = tours.find((el) => el.id === id);
 
-  res.status(200).json({
-    status: 'success',
-    data: { tour },
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: { tour },
+  // });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'sucess',
-        data: { tour: newTour },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'sucess',
+    // data: { tour: newTour },
+  });
 };
 
 exports.updateTour = (req, res) => {
