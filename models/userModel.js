7e4c831c6ec4176ba82e53c10bc353293af7d0b9
Ryan Webb <65420906+Ryan-Webb62@users.noreply.github.com>
2020-11-02 08:31:30 -0500

    Create User model

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const validator = require('validator');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: { type: String },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  paswordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
