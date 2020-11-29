69be854733c71618b44c93aaed2bac4048aa520c
Ryan Webb <65420906+Ryan-Webb62@users.noreply.github.com>
2020-11-29 10:55:19 -0500

    Implement security http headers with helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,11 +11,17 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //1.) Global Middleware
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -23,9 +30,13 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.reqTime = new Date().toLocaleDateString();
   // console.log(req.headers);
