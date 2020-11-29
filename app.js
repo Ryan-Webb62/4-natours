18d3318d1fd701cbd427487ae8d9907de40640a0
Ryan Webb <65420906+Ryan-Webb62@users.noreply.github.com>
2020-11-29 10:44:42 -0500

    implement rate limiting for API routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,16 +9,19 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//1.) Middleware
+//1.) Global Middleware
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
-
-  app.use((req, res, next) => {
-    console.log('Hello from the middleware!');
-    next();
-  });
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
