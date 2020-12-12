ce30dc39c19220acbe23676a503a05b06ffe2fa4
Ryan <65420906+Ryan-Webb62@users.noreply.github.com>
2020-12-12 16:30:38 -0500

    Implement create review and get all reviews routes

---
+++ controllers/reviewController.js
@@ -0,0 +1,33 @@
+const Review = require('../models/reviewModel');
+const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+// Make a new review
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: { review: newReview },
+  });
+});
+
+// Get all reviews
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Review.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  // EXECUTE QUERY
+  const reviews = await features.query;
+
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: {
+      reviews: reviews,
+    },
+  });
+});
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,6 +32,15 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// Query Middelware
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'user',
+    select: '-__v',
+  });
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
