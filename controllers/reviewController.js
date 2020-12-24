d9f06b4fa937b2297a25b6525ec67caba94461e2
Ryan <65420906+Ryan-Webb62@users.noreply.github.com>
2020-12-24 13:04:56 -0500

    Implement post review on tour route

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -5,6 +5,10 @@ const catchAsync = require('../utils/catchAsync');
 
 // Make a new review
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const newReview = await Review.create(req.body);
   res.status(201).json({
     status: 'success',
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
+
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
@@ -24,6 +26,18 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
+  );
+
+// POST /tour/234fad4/reviews
+// GET /tour/234fad4/reviews
+// GET /tour/234fad4/reviews/94887fda
+
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
   );
 
 module.exports = router;
